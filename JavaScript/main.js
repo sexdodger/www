5928734a17d3a7c4e12b997cbcb33eb95aea9186
starter education <gladkiy1518@gmail.com>
2021-07-12 12:52:03 +0300

    my first project

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -94,8 +94,16 @@ console.log(man.virgin && check);
 // != - знак нерівності
 // !check - зміна тру або фолс на протилежне
 // === - перевірка не тільки за значенням а й за типом данних
-console.log("fafafafaf");
-let nameeee;
-let agagaggaagag;
-const asd = 11;
-console.log(asd);
+let numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+console.log(personalMovieDB[count]);
+const movie = prompt("Один из последних просмотренных фильмов?", "");
+const rate = prompt("Как вы его оцените?", "");
+personalMovieDB.movies[movie] = rate;
+console.log(personalMovieDB);
