929c1c05bf32a7eda3cbf35370a0095ab363f227
starter education <gladkiy1518@gmail.com>
2021-07-15 14:32:29 +0300

    practice task 2

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,4 +1,11 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+let numberOfFilms;
+function firstQuestion() {
+  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+firstQuestion();
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -6,8 +13,42 @@ let personalMovieDB = {
   genres: [],
   privat: false,
 };
-console.log(personalMovieDB.count);
-const movie = prompt("Один из последних просмотренных фильмов?", "");
-const rate = prompt("Как вы его оцените?", "");
-personalMovieDB.movies[movie] = rate;
+function rememberMyFilms () {
+for (let i = 0; i < 1; i++) {
+  const movie = prompt("Один из последних просмотренных фильмов?", "");
+  const rate =  prompt("Как вы его оцените?", "");
+  if (movie != null && rate != null && movie != "" && rate != "" && movie.length < 50 && rate.length < 50) {
+    personalMovieDB.movies[movie] = rate;
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+}
+rememberMyFilms();
 console.log(personalMovieDB);
+function showLevel() {
+if (personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
+  alert("Просмотрено слишком мало фильмов");
+} else if (personalMovieDB.count < 30 && personalMovieDB.count > 10) {
+   alert("вы обычный зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы - киноман");
+} else {
+  alert("Произошла ошибка");
+}
+}
+showLevel();
+function showMyDB() {
+  if (personalMovieDB.privat == false) {
+  console.log(personalMovieDB);
+}
+}
+showMyDB();
+function favouriteGenres() {
+  for (let i = 1; i <= 3; i++) {
+  personalMovieDB.genres[i - 1] = prompt("Ваш любимый жанр?");
+}
+}
+favouriteGenres();
+console.log(personalMovieDB.genres);
